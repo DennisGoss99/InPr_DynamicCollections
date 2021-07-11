ee5be73979b290212a3d3b29cd791980fd8ec015
BitPaw <NozsBitPaw@gmail.com>
2021-07-11 18:47:03 +0200

    List copied from BS

--- DynamicCollections/DynamicCollections/BaseContainer/List.c
+++ DynamicCollections/DynamicCollections/BaseContainer/List.c
@@ -1,1 +1,152 @@
 #include "List.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ListInitialize(List* list, unsigned int count, unsigned int sizeOfSingleElement)
+{
+	if (list->Content != NULL || list->SizeOfSingleElement != 0)
+		return;
+
+	list->Content = calloc(count, sizeOfSingleElement);
+	list->Size = count;
+	list->SizeOfSingleElement = sizeOfSingleElement;
+}
+
+void ListDestruction(List* list)
+{
+	if (list == NULL) // Parameter NULL
+		return;
+
+	if (list->Content == NULL) // No data to delete
+		return;
+
+	for (int i = 0; i < list->Size; ++i)
+	{
+		void* element = list->Content[i];
+
+		if (element != NULL)
+		{
+			free(element);
+			list->Content[i] = NULL;
+		}
+	}
+
+	free(list->Content);
+	list->Content = NULL;
+}
+
+int ListItemInsertAt(List* list, unsigned int indexValue, void* value)
+{
+	if (list == NULL)
+		return -1;
+
+	if (indexValue < 0 || indexValue >(list->Size - 1))
+		return -2;
+
+	list->Content[indexValue] = value;
+
+	return 0;
+}
+
+void* ListItemGet(List* list, unsigned int index)
+{
+	if (index > list->Size - 1)
+		return NULL;
+
+	if (list == NULL)
+		return NULL;
+
+	return list->Content[index];
+}
+
+int ListItemAdd(List* list, void* value)
+{
+	if (list == NULL)
+		return -1;
+
+	int unsigned addLocation = 0;
+
+	while (list->Content[addLocation] != NULL && addLocation <= list->Size - 1)
+		addLocation++;
+
+
+	if (addLocation >= list->Size)
+	{
+		const unsigned int oldSize = list->Size;
+
+		list->Size = (int)(1.5 * (list->Size + 1));
+
+
+		void* reallocOutput = realloc(list->Content, list->Size * list->SizeOfSingleElement);
+
+		if (reallocOutput == NULL)
+			return -9;
+
+
+		list->Content = reallocOutput;
+
+		for (int i = oldSize; i < list->Size; ++i)
+			list->Content[i] = NULL;
+	}
+
+	return ListItemInsertAt(list, addLocation, value);
+}
+
+int ListItemRemove(List* list, unsigned int index)
+{
+	if (index > list->Size - 1)
+		return -2;
+
+	if (list == NULL)
+		return -1;
+
+	if (list->Content[index] == NULL)
+		return 1;
+
+	if (list->Content[index] != NULL)
+	{
+		free(list->Content[index]);
+		list->Content[index] = NULL;
+	}
+
+	return 0;
+}
+
+int ListClear(List* list)
+{
+	if (list == NULL)
+		return -1;
+
+	for (int i = 0; i < list->Size; ++i)
+		ListItemRemove(list, i);
+
+	return 0;
+}
+
+void ListPrint_string(List* list)
+{
+	printf("{");
+	for (int i = 0; i < list->Size; ++i)
+	{
+		if (i != 0)
+			printf(",");
+
+		printf("%s", ListItemGet(list, i));
+	}
+	printf("}");
+}
+
+void ListPrint_int(List* list)
+{
+	printf("{");
+	for (int i = 0; i < list->Size; ++i)
+	{
+		if (i != 0)
+			printf(",");
+
+		printf("%i", ListItemGet(list, i));
+	}
+	printf("}");
+}
--- DynamicCollections/DynamicCollections/BaseContainer/List.h
+++ DynamicCollections/DynamicCollections/BaseContainer/List.h
@@ -3,7 +3,84 @@
 
 typedef struct List_
 {
-	unsigned int Length;
+	void** Content;
+	unsigned int SizeOfSingleElement;
+	unsigned int Size;
 }List;
+
+/*
+ * Function:  ListInitialize
+ * --------------------
+ * Acts like a constructor
+ * ! A list must be set to EMPTYLIST before each initialization
+ * eg: 'List a = EMPTYLIST;'
+ * --------------------
+ */
+void ListInitialize(List* list, unsigned int count, unsigned int sizeOfObject);
+
+void ListDestruction(List* path);
+
+/*
+ * Function:  ListItemInsertAt
+ * --------------------
+ * adds parameter value to index in parameter list->Content
+ * --------------------
+ *  returns: 0 if successful
+ *	  -1 List is NULL
+ *	  -2 Out of bounds
+ */
+int ListItemInsertAt(List* list, unsigned int indexValue, void* value);
+
+/*
+ * Function:  ListItemGet
+ * --------------------
+ * adds parameter value to index in parameter list->Content
+ * --------------------
+ *  returns: Value if successful
+ *	  NULL List is NULL
+ *	  NULL Out of bounds
+ */
+void* ListItemGet(List* list, unsigned int index);
+
+/*
+ * Function:  ListItemAdd
+ * --------------------
+ * adds parameter value to next free value in parameter list->Content
+ * --------------------
+ *
+ * When the List is full, it will be made bigger
+ *
+ *  returns: 0 if successful
+ *	  -1 List is NULL
+ *	  -2 Out of bounds
+ *	  -9 Calloc failed
+ */
+int ListItemAdd(List* list, void* value);
+
+/*
+ * Function:  ListItemRemove
+ * --------------------
+ * adds parameter value to index in parameter list->Content
+ * --------------------
+ *  returns: 0 if successful
+ *     1 Removed Value already NULL
+ *	  -1 List is NULL
+ *	  -2 Out of bounds
+ */
+int ListItemRemove(List* list, unsigned int index);
+
+/*
+ * Function:  ListClear
+ * --------------------
+ * sets all values in list->Content = 0
+ * --------------------
+ *  returns: 0 if successful
+ *	  -1 List is NULL
+ */
+int ListClear(List* list);
+
+void ListPrint_string(List* list);
+
+void ListPrint_int(List* list);
 
 #endif
